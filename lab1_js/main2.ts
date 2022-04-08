1066378ffa1e6d1c0d923e92bda3ef599f79de5e
Kniites <kniites@gmail.com>
2022-04-08 12:01:08 +1000

    lab1 done

---
+++ lab1_js/main2.ts
@@ -0,0 +1,7 @@
+let num1: number = 1;
+let num2: number = 2;
+let num3: number = 3;
+let result: number = num1 + num2 + num3;
+let stri1: string = 'Sum of numbers is: ';
+let message = `${stri1}${result}!`;
+console.log(message);
